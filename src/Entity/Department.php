64d8848acdc661c1b274c98b995e683679b5b0a1
thanhdung96 <14dh110224@st.huflit.edu.vn>
2023-08-16 15:43:32 +0700

    Added base entities.
    Changes to be committed:
    	modified:   .gitignore
    	modified:   .idea/graph.iml
    	modified:   .idea/php.xml
    	new file:   .idea/phpspec.xml
    	new file:   .idea/phpunit.xml
    	modified:   composer.json
    	modified:   composer.lock
    	modified:   config/bundles.php
    	new file:   config/packages/graphql.yaml
    	new file:   config/packages/uid.yaml
    	new file:   migrations/Version20230816083914.php
    	new file:   migrations/Version20230816084210.php
    	new file:   src/Entity/Department.php
    	new file:   src/Entity/Device.php
    	new file:   src/Entity/Employee.php
    	new file:   src/Repository/DepartmentRepository.php
    	new file:   src/Repository/DeviceRepository.php
    	new file:   src/Repository/EmployeeRepository.php
    	modified:   symfony.lock

---
+++ src/Entity/Department.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DepartmentRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: DepartmentRepository::class)]
+class Department
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(length: 36, unique: true)]
+    private string $id;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\OneToMany(mappedBy: 'department', targetEntity: Employee::class)]
+    private Collection $supervisors;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $created = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $modified = null;
+
+    #[ORM\OneToMany(mappedBy: 'department', targetEntity: Employee::class)]
+    private Collection $members;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $deleted = null;
+
+    public function __construct()
+    {
+        $this->supervisors = new ArrayCollection();
+        $this->members = new ArrayCollection();
+
+        $timestamp = new \DateTime();
+        $this->created = $timestamp;
+        $this->modified = $timestamp;
+        $this->id = Uuid::v7()->generate($timestamp);
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Employee>
+     */
+    public function getSupervisors(): Collection
+    {
+        return $this->supervisors;
+    }
+
+    public function addSupervisor(Employee $supervisor): static
+    {
+        if (!$this->supervisors->contains($supervisor)) {
+            $this->supervisors->add($supervisor);
+            $supervisor->setDepartmentSupervised($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSupervisor(Employee $supervisor): static
+    {
+        if ($this->supervisors->removeElement($supervisor)) {
+            // set the owning side to null (unless already changed)
+            if ($supervisor->getDepartmentSupervised() === $this) {
+                $supervisor->setDepartmentSupervised(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getCreated(): ?\DateTimeInterface
+    {
+        return $this->created;
+    }
+
+    public function setCreated(\DateTimeInterface $created): static
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    public function getModified(): ?\DateTimeInterface
+    {
+        return $this->modified;
+    }
+
+    public function setModified(\DateTimeInterface $modified): static
+    {
+        $this->modified = $modified;
+
+        return $this;
+    }
+
+    public function getDeleted(): ?\DateTimeInterface
+    {
+        return $this->deleted;
+    }
+
+    public function setDeleted(?\DateTimeInterface $deleted): static
+    {
+        $this->deleted = $deleted;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Employee>
+     */
+    public function getMembers(): Collection
+    {
+        return $this->members;
+    }
+
+    public function addMember(Employee $member): static
+    {
+        if (!$this->members->contains($member)) {
+            $this->members->add($member);
+            $member->setDepartment($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMember(Employee $member): static
+    {
+        if ($this->members->removeElement($member)) {
+            // set the owning side to null (unless already changed)
+            if ($member->getDepartment() === $this) {
+                $member->setDepartment(null);
+            }
+        }
+
+        return $this;
+    }
+}
